9b1421f7fb822d7df63ca01f68b0b156775cfc63
Maritere <mtnietogalan@gmail.com>
2023-02-25 11:28:49 +0100

    nft collection finished

--- collection/TicketCollection.sol
+++ collection/TicketCollection.sol
@@ -5,21 +5,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract TicketCollection is ERC721, ERC721Enumerable, Ownable {
+contract VampireWeekendTickets is ERC721, ERC721Enumerable, Ownable {
     // Optional mapping for token URIs
     mapping(uint256 => string) private _tokenURIs;
 
-    address private MINT_DESTINATION = "";  // Mint address owner
+    address private MINT_DESTINATION = 0x8c091cfeA36b48F4a5ac022677043601308a521e;  // Mint address owner
     
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
         // Minting the tickets when the contract is deployed
-        for (uint i = 0; i <= 9; i++){
+        for (uint i = 0; i <= 3; i++){
             _safeMint(MINT_DESTINATION, i);
         }
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://{{IPFS CID}}/";
+        return "ipfs://bafybeieh2xox4zi7eoc2fabjs4ywasjle62sqkt7ofx7kv2sfpi6ndi3d4/nft/";
     }
 
     /**
---
+++ collection/collection.md
@@ -0,0 +1,6 @@
+ipfs-car --pack tickets --output tickets.car
+
+
+ipfs://bafybeigpl5wxnsouivmuipmdwl2wvebvjeauw3mmrg7ypdkbjxpn77ukoa
+
+bafybeigpl5wxnsouivmuipmdwl2wvebvjeauw3mmrg7ypdkbjxpn77ukoa
